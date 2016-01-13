cc3d40e2839e78f205b9a85e7149fd3e840b17eb
Naser Ahmed <naser.ahmed@heg.com>
2016-01-13 09:53:21 +0000

    updating homepage v3

--- src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
-	String message = "Welcome to Nasers Spring MVC Web App. LMAO!";
+	String message = "Welcome to Nasers Spring MVC Web App. LMAO! and Hello again";
 
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
